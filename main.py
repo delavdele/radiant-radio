1a17e069ab32a968de3f60f0d39386039203fadf
delavdele <123886551+delavdele@users.noreply.github.com>
2023-07-11 14:11:52 +0300

    Add new html, modify main.py (#2)

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'hello test'
+def index():
+    draft_variable = 'I want to listen to this music forever!'
+    musical_sets = [{'time': '11.07.23',
+                  'head': 'TRANSATLANTIC',
+                  'body': 'Feminism'
+                  },
+                    {'time': '12.07.23',
+                     'head': 'TRANS',
+                     'body': 'Fusion'
+                     },
+                    {'time': '13.07.23',
+                     'head': 'TRANSMOTION',
+                     'body': 'Fem'
+                     },
+                    {'time': '14.07.23',
+                     'head': 'TEHNO',
+                     'body': 'Freejazz'
+                     }]
+    return render_template('index.html', dv=draft_variable, musical_sets=musical_sets)
 
 
 if __name__ == '__main__':
