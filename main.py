e03a62ebb11f08c85f32128628198752af2ee60f
Adel Yudina <adelyudina@gmail.com>
2023-07-21 15:12:20 +0300

    add images

--- main.py
+++ main.py
@@ -9,22 +9,26 @@ def index():
     musical_sets = [{'time': '11.07.23',
                      'head': 'TRANSATLANTIC',
                      'body': 'Feminism',
-                     'file': 'audio/amorsatyr.mp3'
+                     'file': 'audio/amorsatyr.mp3',
+                     'image': 'images/cat.jpg'
                      },
                     {'time': '12.07.23',
                      'head': 'TRANS',
                      'body': 'Fusion',
                      'file': 'audio/laparole.mp3',
+                     'image': 'images/gamescreen.png'
                      },
                     {'time': '13.07.23',
                      'head': 'TRANSMOTION',
                      'body': 'Fem',
                      'file': 'audio/money.mp3',
+                     'image': 'images/octopus.jpg'
                      },
                     {'time': '14.07.23',
                      'head': 'TEHNO',
                      'body': 'Freejazz',
                      'file': 'audio/umamami.mp3',
+                     'image': 'images/P.jpg'
                      }]
     return render_template('index.html', title=draft_variable, musical_sets=musical_sets)
 
